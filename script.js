ab97bab68ef5b8f4fafbbb4460b07c0784646915
AdminK <volvenkova.k@gmail.com>
2023-06-23 23:30:05 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,94 @@
+// Реалізувати подобу інтернет-магазину.
+
+// Дано 3 блоки
+
+// В лівій частині сторінки - перелік категорій.
+// При натисканні на категорію в середній блок виводиться список товарів цієї категорії.
+// Клік на товар - інформацію про товар у правому блоці.
+// В інформації товару - кнопка “купити”.
+// При натисканні на “купити” з'являється повідомлення, 
+// що товар куплений і повернення у вихідний стан програми ( коли відображається лише список категорій).
+
+
+let mainCatalog = document.querySelectorAll('.catalogItem');
+let products = document.querySelectorAll('.products');
+
+let productsArray = document.querySelectorAll('.productCard');
+let infoBlocks = document.querySelectorAll('.productInfo');
+
+let buyBtn = document.getElementById('buyBtn');
+
+mainCatalog.forEach((catalogItem) => {
+    catalogItem.addEventListener('click', (event) => {
+        let target = event.target;
+        const productsListId = target.getAttribute('data-block');
+
+        products
+            .forEach((catalogCategory) => {
+                catalogCategory.style.display = 'none';
+
+                if (catalogCategory.id === productsListId) {
+                    catalogCategory.style.display = 'flex';
+                }
+            })
+
+        infoBlocks
+            .forEach((infoBlock) => {
+                infoBlock.style.display = 'none';
+            })
+
+        buyBtn.style.display = 'none';
+    }
+    )
+})
+
+
+productsArray.forEach((product) => {
+    product.addEventListener('click', (event) => {
+        let target = event.target;
+        const productInfoId = target.getAttribute('data-block');
+
+        infoBlocks
+            .forEach((infoBlock) => {
+                infoBlock.style.display = 'none';
+
+                if (infoBlock.id === productInfoId) {
+                    infoBlock.style.display = 'block';
+                }
+            })
+
+        buyBtn.style.display = 'block';
+    }
+    )
+})
+
+
+
+buyBtn.addEventListener('click', () => {
+    let message = document.createElement('p');
+    message.classList.add('message');
+    message.textContent = 'Товар куплений';
+    document.getElementById('productsInfoCards').appendChild(message);
+
+    buyBtn.setAttribute('disabled', 'disabled');
+
+    setTimeout(() => {
+        infoBlocks
+            .forEach((infoBlock) => {
+                infoBlock.style.display = 'none';
+            })
+
+        buyBtn.style.display = 'none';
+
+        products
+            .forEach((catalogCategory) => {
+                catalogCategory.style.display = 'none';
+
+            })
+
+        message.style.display = 'none';
+        buyBtn.removeAttribute('disabled');
+    }, 2000)
+
+
+})
